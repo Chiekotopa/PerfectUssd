d37e434713d16833ed424165b355b34ceaf14d4e
tchiozem <sophos.tc7@gmail.com>
2020-11-30 16:26:45 +0100

    mise à jour dans le controler et le service

--- src/main/java/com/ussd/pay/controler/UssdRestController.java
+++ src/main/java/com/ussd/pay/controler/UssdRestController.java
@@ -6,7 +6,10 @@
 package com.ussd.pay.controler;
 
 import com.ussd.pay.pojo.PojoUssd;
+import com.ussd.pay.pojo.Responses;
+import com.ussd.pay.service.UssdService;
 import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,51 +25,75 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UssdRestController {
 
+    @Autowired
+    UssdService ussdservice;
+
+
     @RequestMapping(value = "/ussd", method = RequestMethod.POST)
     public HashMap getUssd(@RequestBody PojoUssd pojoUssd) {
         HashMap map = new HashMap();
         if (pojoUssd.getMessage().equals("237*100")) {
-            map.put("message","Bienvenue sur PerfectPay~1.Transfert d'argent~2.Paiement Marchand~3.Operations Bancaires~4.Services Tiers~5.Mon compte~0.Annuler ");
+
+            map.put("message", "Bienvenue sur PerfectPay~1.Transfert d'argent~2.Paiement Marchand~3.Operations Bancaires~4.Services Tiers~5.Mon compte~0.Annuler ");
             map.put("command", "1");
             return map;
         }
-        
-         if (pojoUssd.getMessage().equals("1")) {
-            map.put("message","Transfert d'argent~1.Vers un client PerfectPay~2.Vers un client MTN~3.Vers un client Orange~4.Vers un client EU MobileMoney~5.Vers un client YUP~6.Vers un client YUP~0.Retour ");
+
+        if (pojoUssd.getMessage().equals("1")) {
+            Responses  response=new Responses();
+            response= ussdservice.CheckphoneNumber(pojoUssd.getMsisdn());
+           if(response.getSucces()==-1){
+              map.put("message", response.getMsg());
+              map.put("command", "1");
+              return map;
+           }
+            if(response.getSucces()==0){
+              map.put("message", response.getMsg());
+              map.put("command", "1");
+              return map;
+           }
+            map.put("message", "Transfert d'argent~1.Vers un client PerfectPay~2.Vers un client MTN~3.Vers un client Orange~4.Vers un client EU MobileMoney~5.Vers un client YUP~6.Vers un client YUP~0.Retour ");
             map.put("command", "1");
             return map;
         }
-         
-          if (pojoUssd.getMessage().equals("2")) {
-            map.put("message","Paiement Marchand~1.Nouveau Paiement~2.Checker un code marchand~0.Retour");
+
+        if (pojoUssd.getMessage().equals("2")) {
+            map.put("message", "Paiement Marchand~1.Nouveau Paiement~2.Checker un code marchand~0.Retour");
             map.put("command", "1");
             return map;
         }
-          
-           if (pojoUssd.getMessage().equals("3")) {
-            map.put("message","Selectionner votre banque parmi nos banques partenaires~1.UBA~2.Afriland~3.Banque Atlantique~4.Ecobank~5.Societe Generale~0.Retour ");
+
+        if (pojoUssd.getMessage().equals("3")) {
+            map.put("message", "Selectionner votre banque parmi nos banques partenaires~1.UBA~2.Afriland~3.Banque Atlantique~4.Ecobank~5.Societe Generale~0.Retour ");
             map.put("command", "1");
             return map;
         }
-           
-           if (pojoUssd.getMessage().equals("4")) {
-            map.put("message","Services Tierces~1.Payez un abonnement GEDOMED~2.Payez une licence IPLANS ERP~3.Payer Cotisation ONMC~4.Payer Frais etude de dossier ONMC~5.Payer Frais de siège ONMC~6.Payer Facture ENEO~7.Payer Facture CAMWATER");
+
+        if (pojoUssd.getMessage().equals("4")) {
+            map.put("message", "Services Tierces~1.Payez un abonnement GEDOMED~2.Payez une licence IPLANS ERP~3.Payer Cotisation ONMC~4.Payer Frais etude de dossier ONMC~5.Payer Frais de siège ONMC~6.Payer Facture ENEO~7.Payer Facture CAMWATER");
             map.put("command", "1");
             return map;
         }
-           
-            if (pojoUssd.getMessage().equals("5")) {
-            map.put("message","Mon compte PerfectPay~1.Consulter solde~2.Modifier Code PIN~3.Consulter dernières transactions~4.Changer de langue~0.Retour ");
+
+        if (pojoUssd.getMessage().equals("5")) {
+            map.put("message", "Mon compte PerfectPay~1.Consulter solde~2.Modifier Code PIN~3.Consulter dernières transactions~4.Changer de langue~0.Retour ");
             map.put("command", "1");
             return map;
         }
-            
-             if (pojoUssd.getMessage().equals("0")) {
-            map.put("message","Operation annulee! ");
+
+        if (pojoUssd.getMessage().equals("0")) {
+            map.put("message", "Operation annulee! ");
             map.put("command", "0");
             return map;
         }
 
         return map;
+    }
+    
+     @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public Responses getUssd() {
+      Responses response=new Responses();
+      response=  ussdservice.CheckphoneNumber("691788864");
+        return response ;
     }
 }
--- src/main/java/com/ussd/pay/service/UssdService.java
+++ src/main/java/com/ussd/pay/service/UssdService.java
@@ -5,7 +5,14 @@
  */
 package com.ussd.pay.service;
 
+import com.ussd.pay.pojo.Responses;
+import java.util.HashMap;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -13,13 +20,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UssdService {
-    
-    public void transfertOrangeMoney(){
-        
+
+    HttpHeaders headers = new HttpHeaders();
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    public void transfertToClientPerfectPay() {
+        HashMap hashMap = new HashMap();
+
     }
-    
-    public void transfertToClientPerfectPay(){
-        
+
+    public Responses CheckphoneNumber(String phone) {
+        Responses response = new Responses();
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        String boString="";
+        headers.set(HttpHeaders.ACCEPT, "application/json");
+        HttpEntity<Responses> entity = new HttpEntity<>(response, headers);
+
+        String url = "http://154.72.148.105/apipayment/api-perfectpay.php?action=checker_compte_expediteur&Code_client=" + phone + "";
+       response = restTemplate.getForObject(url, Responses.class, response);
+     
+        return response;
     }
-    
+
+    public String Checksolde(String solde) {
+
+        return "";
+    }
+
 }
